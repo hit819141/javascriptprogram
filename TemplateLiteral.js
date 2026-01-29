5e6b39f95f2931eaf3ba26f666006a40d26ddf5e
Bhalodiya Hit <bhalodiyahit21@gmail.com>
2026-01-29 09:44:03 +0530

    [HB] [string method & templateliter]

---
+++ TemplateLiteral.js
@@ -0,0 +1,4 @@
+let fname = "Hit";
+let lname = "Bhalodiya";
+let message = `Hello student, i am ${fname} ${lname}.`;
+console.log(message);
---
+++ method.js
@@ -0,0 +1,36 @@
+//---string method---
+//--length()---
+//---trim()---
+//---toUpperCase()---
+//---toLowerCase()---
+//---includes()---
+//---indexOf()---
+//---slice()---
+//---substring()---
+//---replace()---
+//--concat()---
+//---split()---
+
+let str = "  welcome, billy bowdan ❤️  ";
+console.log(str);
+console.log("length() :", str.length);
+
+let trimtext = str.trim();
+console.log("trim():", trimtext);
+
+console.log("toUpperCase():", str.toUpperCase());
+console.log("toLowerCase():", str.toLowerCase());
+
+console.log("includes():", str.includes("billy"));
+
+console.log("indexOf():", str.indexOf("bowdan"));
+
+console.log("slice():", str.slice(2, 9));
+
+console.log("substring():", str.substring(2, 9));// they are in a negative value are a not useable
+
+console.log("replace():", str.replace("welcome", "hello"));
+
+console.log("concat():", str.concat(" Have a nice day!"));
+
+console.log("split():", str.split(" "));
