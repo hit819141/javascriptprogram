6d72dccde07f29dd8daf069ac04399a610ac7fc1
Bhalodiya Hit <bhalodiyahit21@gmail.com>
2026-01-29 13:29:39 +0530

    object file updated

--- objectmethod.js
+++ objectmethod.js
@@ -1,5 +1,7 @@
 // object is the collection of any type of data
 
+// const { use } = require("react");
+
 const person = {
     name: "Billy",
     age: 20,
@@ -37,3 +39,49 @@ console.log(person1?.address?.city)
 console.log(Object.keys(person1))
 console.log(Object.values(person1))
 console.log(Object.entries(person1))
+
+const user={
+    name:"billy",
+    role:"Admin",
+    designation:"devloper",
+    age:"20",
+    address:{
+        city:"keshod",
+        zipcode:"362001",
+        street:"123 main st."
+    }
+}
+const {name : profileName ,age: profileAge}= user
+console.log(profileName)
+console.log(profileAge)
+
+const {name,age,role,state="Gujarat"}=user
+console.log(name,age,role,state)
+
+const array =[1,2,3]
+const moreElements = [...array,4,5,6]
+console.log(moreElements)
+
+const originalArray = [1,2,3]
+const copiedArray = [...originalArray]
+console.log(copiedArray)
+
+const arr1 = [1,2,3]
+const aar2 = [4,5,6]
+const mergedArray=[...arr1,...aar2]
+console.log(mergedArray)
+
+const user1 ={
+    name : "Hit Bhalodiya",
+    city : "keshod",
+}
+const user2 ={
+    address : "keshod",
+    age :20,
+}
+
+const newObj = {
+    ...user1,
+    ...user2,
+}
+console.log(newObj)
